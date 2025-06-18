4a468caa7e1027efe3465f54ad79fd666e2ceb23
Khushboo Neema <khushboo.neema@wundermanthompson.com>
2025-06-18 11:14:24 -0400

    Initial Commit

---
+++ SwiftDataSampleApp/SwiftDataSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataSampleAppApp.swift
+//  SwiftDataSampleApp
+//
+//  Created by Khushboo Neema on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDataSampleAppTests/SwiftDataSampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataSampleAppTests.swift
+//  SwiftDataSampleAppTests
+//
+//  Created by Khushboo Neema on 6/18/25.
+//
+
+import Testing
+@testable import SwiftDataSampleApp
+
+struct SwiftDataSampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
